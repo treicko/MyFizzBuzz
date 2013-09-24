8e0551d4e91f300ddcd752da55cc7fb424ed1011
leo <leo_mejia_p@hotmail.com>
2013-09-24 06:48:22 -0400

    se aniade servler para la configuracoin necesrio par auqe funcione

---
+++ src/main/java/MyFizzBuzz/FizzBuzzServlet.java
@@ -0,0 +1,16 @@
+package MyFizzBuzz;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FizzBuzzServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().println("Servlet vivo!!");
+	}
+}
